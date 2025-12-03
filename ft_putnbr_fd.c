ad2b11d3da3cf981c95fc2e8276806b2adf98708
dhayse.tito <dhaysetito@gmail.com>
2025-12-02 22:39:10 -0300

    fiz: norminette

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,13 +6,13 @@
 /*   By: dhde-lim <dhde-lim@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 01:42:55 by dhde-lim          #+#    #+#             */
-/*   Updated: 2025/12/02 22:35:42 by dhde-lim         ###   ########.fr       */
+/*   Updated: 2025/12/02 22:38:52 by dhde-lim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
 		return (ft_putstr_fd("-2147483648", fd));
