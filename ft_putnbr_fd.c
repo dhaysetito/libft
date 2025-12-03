9e3f77f4e4f56c88f63484c823d15409ec54ce8c
dhayse.tito <dhaysetito@gmail.com>
2025-12-02 22:36:56 -0300

    fix: putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,17 +6,22 @@
 /*   By: dhde-lim <dhde-lim@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 01:42:55 by dhde-lim          #+#    #+#             */
-/*   Updated: 2025/11/12 01:52:46 by dhde-lim         ###   ########.fr       */
+/*   Updated: 2025/12/02 22:35:42 by dhde-lim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void ft_putnbr_fd(int n, int fd)
 {
-	char	*nbr;
-
-	nbr = ft_itoa(n);
-	ft_putstr_fd(nbr, fd);
-	free(nbr);
+	if (n == -2147483648)
+		return (ft_putstr_fd("-2147483648", fd));
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	if (n >= 10)
+		ft_putnbr_fd(n / 10, fd);
+	ft_putchar_fd((n % 10) + 48, fd);
 }
